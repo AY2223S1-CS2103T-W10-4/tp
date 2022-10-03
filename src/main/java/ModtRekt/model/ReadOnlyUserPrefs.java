988db3628ab4c0312bf50783641ea066aaebb90b
Marciano Renzo William <marcianorenzowilliam@gmail.com>
2022-10-03 22:15:35 +0800

    Rename package to ModtRekt

---
+++ src/main/java/ModtRekt/model/ReadOnlyUserPrefs.java
@@ -0,0 +1,16 @@
+package ModtRekt.model;
+
+import java.nio.file.Path;
+
+import ModtRekt.commons.core.GuiSettings;
+
+/**
+ * Unmodifiable view of user prefs.
+ */
+public interface ReadOnlyUserPrefs {
+
+    GuiSettings getGuiSettings();
+
+    Path getAddressBookFilePath();
+
+}
---
+++ src/test/java/ModtRekt/commons/util/FileUtilTest.java
@@ -0,0 +1,24 @@
+package ModtRekt.commons.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ModtRekt.testutil.Assert.assertThrows;
+
+import ModtRekt.testutil.Assert;
+import org.junit.jupiter.api.Test;
+
+public class FileUtilTest {
+
+    @Test
+    public void isValidPath() {
+        // valid path
+        assertTrue(FileUtil.isValidPath("valid/file/path"));
+
+        // invalid path
+        assertFalse(FileUtil.isValidPath("a\0"));
+
+        // null path -> throws NullPointerException
+        Assert.assertThrows(NullPointerException.class, () -> FileUtil.isValidPath(null));
+    }
+
+}
